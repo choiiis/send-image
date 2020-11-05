65e94cd071c934c231b3b243d06ec5d42a3e3068
unknown <haemin_choi@lotte.net>
2020-11-05 17:54:02 +0900

    backup --1105

--- app/src/main/java/com/example/sendimage/MainActivity.kt
+++ app/src/main/java/com/example/sendimage/MainActivity.kt
@@ -1,89 +1,88 @@
 package com.example.sendimage
-
-import android.Manifest.permission.CAMERA
-import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.app.Activity
 import android.content.Intent
-import android.content.pm.PackageManager;
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.media.ExifInterface
 import android.net.Uri
-import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
+import android.widget.Button
 import android.widget.ImageView
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.content.FileProvider
+
+import com.gun0912.tedpermission.TedPermission
 import java.io.File
+import com.gun0912.tedpermission.PermissionListener
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    val REQUEST_IMAGE_CAPTURE = 1
+    lateinit var currentPhotoPath : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var camerButton: ImageView
-        camerButton = findViewById(R.id.camera_button)
+        var btn_picture: Button
+        btn_picture = findViewById(R.id.btn_picture)
 
-        camerButton.setOnClickListener {
-            if(checkPersmission()) {
-                dispatchTakePictureIntent()
+
+        settingPermission() // 권한체크 시작
+
+        btn_picture.setOnClickListener {
+            startCapture()
+        }
+
+    }
+
+    fun settingPermission(){
+        var permis = object  : PermissionListener{
+            //            어떠한 형식을 상속받는 익명 클래스의 객체를 생성하기 위해 다음과 같이 작성
+            override fun onPermissionGranted() {
+                Toast.makeText(this@MainActivity, "권한 허가", Toast.LENGTH_SHORT)
+                        .show()
             }
-            else {
-                requestPermission()
+
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                Toast.makeText(this@MainActivity, "권한 거부", Toast.LENGTH_SHORT)
+                        .show()
+                ActivityCompat.finishAffinity(this@MainActivity) // 권한 거부시 앱 종료
             }
         }
+
+        TedPermission.with(this)
+                .setPermissionListener(permis)
+                .setRationaleMessage("카메라 사진 권한 필요")
+                .setDeniedMessage("카메라 권한 요청 거부")
+                .setPermissions(
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+//                android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                        android.Manifest.permission.CAMERA)
+                .check()
     }
 
-    val REQUEST_IMAGE_CAPTURE = 1
-
-    // 카메라 권한 요청
-    private fun requestPermission() {
-        ActivityCompat.requestPermissions(this, arrayOf(READ_EXTERNAL_STORAGE, CAMERA),
-            REQUEST_IMAGE_CAPTURE)
-    }
-
-    // 카메라 권한 체크
-    private fun checkPersmission(): Boolean {
-        return (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) ==
-                PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
-            android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
-    }
-
-    // 권한요청 결과
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            Log.d("TAG", "Permission: " + permissions[0] + "was " + grantResults[0] + "카메라 권한 승인")
-        }else{
-            Log.d("TAG","카메라 권한 거부")
-        }
-    }
-
-    // 카메라 열기
-    private fun dispatchTakePictureIntent() {
+    fun startCapture(){
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-            if (takePictureIntent.resolveActivity(this.packageManager) != null) {
-                // 찍은 사진을 그림파일로 만들기
-                val photoFile: File? =
-                    try {
-                        createImageFile()
-                    } catch (ex: IOException) {
-                        Log.d("TAG", "그림파일 만드는도중 에러생김")
-                        null
-                    }
-
-                // 그림파일을 성공적으로 만들었다면 onActivityForResult로 보내기
-                photoFile?.also {
-                    val photoURI: Uri = FileProvider.getUriForFile(
-                        this, "com.example.cameraonly.fileprovider", it
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                val photoFile: File? = try{
+                    createImageFile()
+                }catch(ex:IOException){
+                    null
+                }
+                photoFile?.also{
+                    val photoURI : Uri = FileProvider.getUriForFile(
+                            this,
+                            "org.techtown.capturepicture.fileprovider",
+                            it
                     )
                     takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
                     startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
@@ -92,21 +91,73 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
-    // 카메라로 촬영한 이미지를 파일로 저장해준다
     @Throws(IOException::class)
-    private fun createImageFile(): File {
-        // Create an image file name
-        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+    private fun createImageFile() : File{
+        val timeStamp : String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir : File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
         return File.createTempFile(
-            "JPEG_${timeStamp}_", /* prefix */
-            ".jpg", /* suffix */
-            storageDir /* directory */
-        ).apply {
-            // Save a file: path for use with ACTION_VIEW intents
+                "JPEG_${timeStamp}_",
+                ".jpg",
+                storageDir
+        ).apply{
             currentPhotoPath = absolutePath
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        var img_picture: ImageView
+        img_picture = findViewById(R.id.img_picture)
+
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK){
+            val bitmap = BitmapFactory.decodeFile(currentPhotoPath)
+            lateinit var exif : ExifInterface
+
+            try{
+                exif = ExifInterface(currentPhotoPath)
+                var exifOrientation = 0
+                var exifDegree = 0
+
+                if (exif != null) {
+                    exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                            ExifInterface.ORIENTATION_NORMAL)
+                    exifDegree = exifOrientationToDegress(exifOrientation)
+                }
+
+                img_picture.setImageBitmap(rotate(bitmap, exifDegree))
+            }catch (e : IOException){
+                e.printStackTrace()
+            }
+
+        }
+    }
+
+    private fun exifOrientationToDegress(exifOrientation: Int): Int {
+        when(exifOrientation){
+            ExifInterface.ORIENTATION_ROTATE_90 ->{
+                Log.d("rotate","rotate90")
+                return 90
+            }
+            ExifInterface.ORIENTATION_ROTATE_180 -> {
+                Log.d("rotate","rotate180")
+                return 180
+            }
+            ExifInterface.ORIENTATION_ROTATE_270 ->{
+                Log.d("rotate","rotate270")
+                return 270
+            }
+            else -> {
+                Log.d("rotate","rotate0")
+                return 0
+            }
+
+        }
+    }
+
+    private fun rotate(bitmap: Bitmap, degree: Int) : Bitmap {
+        Log.d("rotate","init rotate")
+        val matrix = Matrix()
+        matrix.postRotate(degree.toFloat())
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix,true)
+    }
 }
